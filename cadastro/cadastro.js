d5ab7f6ed9b5be5876cc08f6474925311f8a8d2d
marcosantoniodesantana <100289385+marcosantoniodesantana@users.noreply.github.com>
2022-03-23 21:15:00 -0300

    Update cadastro.js

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,3 +1,25 @@
+let menubt = document.querySelector('#menu-bar').addEventListener("click", () =>{
+	let menuOptions = document.querySelector('.menu');
+	menuOptions.classList.toggle('visible');
+
+	let menuIcon = document.querySelector(".fa-solid");
+
+	if (menuIcon.classList.contains('fa-bars')) {
+		menuIcon.classList.remove('fa-bars');
+		menuIcon.classList.toggle('fa-xmark');
+		document.querySelector('#menu-bar').classList.toggle('menu-clicado');
+	}else{
+		menuIcon.classList.remove('fa-xmark');
+		menuIcon.classList.toggle('fa-bars');
+		document.querySelector('#menu-bar').classList.remove('menu-clicado');
+	}
+});
+
+let linkConta = document.querySelector('.menu > ul > li:nth-child(1)').addEventListener("click", ()=>{
+	let subMenuOptions = document.querySelector('.submenu');
+		subMenuOptions.classList.toggle('menu-option-visible');
+});
+
 let termosTxt = document.querySelector('.descricao > a');
 termosTxt.addEventListener('click', ()=>{
 	console.log("Termos de uso clicados");
