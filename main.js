07dab090a6f3c0017f7d0210afcf088047f464f8
marcosantoniodesantana <100289385+marcosantoniodesantana@users.noreply.github.com>
2022-04-17 19:31:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -28,8 +28,11 @@
 		if (tituloProduto[i].innerText.length < 28) {
 			tituloProduto[i].classList.toggle("altura");
 		}
+
+
 	}
 
+		let intensVitrine = document.querySelectorAll(".itens");
 //Parte responsavél por fazer a execução do menu
 
 let menubt = document.querySelector('#menu-bar').addEventListener("click", () =>{
@@ -49,6 +52,8 @@ let menubt = document.querySelector('#menu-bar').addEventListener("click", () =>
 	}
 });
 
+//Parte que adiciona a biblioteca de aparecimento gradual com animação
+
 let linkConta = document.querySelector('.menu > ul > li:nth-child(1)').addEventListener("click", ()=>{
 	let subMenuOptions = document.querySelector('.submenu');
 		subMenuOptions.classList.toggle('menu-option-visible');
@@ -59,15 +64,6 @@ document.querySelectorAll(".vitrine > .itens").forEach((iten) =>{
 	iten.setAttribute('data-aos-duration', '2000');
 });
 
-let intensVitrine = document.querySelectorAll(".itens");
-
-for(let iv = 0; iv < intensVitrine.length; iv++){
-	intensVitrine[iv].addEventListener("mouseover", ()=>{
-		//console.log(limite.value)
-		//console.log(iv)
-		//return limite = 40;
-	})
-}
 
 
 let vitrine = document.querySelector('.vitrine');
@@ -81,89 +77,3 @@ document.querySelector('.anterior').addEventListener("click", ()=> {
 });
 
 //IMPORTANTE!!!!!! => https://michalsnik.github.io/aos/
-
-//Script que ativa a biblioteca modificada okzoom que foi editada
-
-
-//Cria uma sombra para a tela de visualização dos produtos
-const infsProdutoSombra = document.querySelector(".tela-infs");
-
-const geralProdutosInf = document.createElement("div");
-geralProdutosInf.classList = "geral-produtos-inf";
-
-const divTop = document.createElement("div");
-divTop.classList = "div-top";
-
-const btnFechar = document.createElement("button");
-btnFechar.classList = "bt-fechar";
-
-const btnFecharIcon = document.createElement("i");
-btnFecharIcon.classList = "fas fa-times";
-
-//divisão com imagem do produto junto a descrição e o preço
-
-const produtoDiv = document.createElement("div");
-produtoDiv.classList = "divisao-produto";
-
-const imgProdutofigure = document.createElement("figure");
-imgProdutofigure.classList = "img-detalhada";
-
-
-let itens = document.querySelectorAll(".itens");
-
-		let selecionaImagem = document.querySelectorAll(".itens > img");
-
-		selecionaImagem.forEach((selecionaImagem) =>{
-			selecionaImagem.id = "imagem-produto";
-		});
-		let imagemClicada = document.querySelectorAll("#imagem-produto");
-
-for (let a = 0; a < itens.length; a++) {
-	itens[a].addEventListener("click", ()=>{
-
-
-		//Isso é o código que faz o zoom
-		$(document).ready(()=>{
-			$('.imagem-detalhada-zoom').okzoom({
-	  		width: 300,
-		  	height: 400,
-		  	round: true,
-	  		background: "transparent",
-		  	shadow: "0 0 0 transparent",
-		  	radius: 10
-		});
-});
-		//Eu achei que esse console.log(itens[a]) não tava funcionando mas pelo visto ele tá
-		console.log(itens[a])
-		infsProdutoSombra.classList.toggle("sombra");
-		document.querySelector(".sombra").appendChild(geralProdutosInf);
-		document.querySelector(".geral-produtos-inf").appendChild(divTop);
-		document.querySelector(".div-top").appendChild(btnFechar);
-		document.querySelector(".bt-fechar").appendChild(btnFecharIcon);
-
-		document.querySelector(".geral-produtos-inf").appendChild(produtoDiv);
-		document.querySelector(".divisao-produto").appendChild(imgProdutofigure);
-
-		//Essa variável a baixo seleciona todos os atributos 'src' das tags img que possuirem o id 'imagem-produtos'
-		const imageDetalhada = document.createElement('img');
-
-		for(let b = 0; b < imagemClicada.length; b++){
-			imageDetalhada.classList = "imagem-detalhada-zoom";
-			//console.log(imagemClicada.length)
-
-			const contadorImagens = document.querySelectorAll(".imagem-detalhada-zoom");
-
-			imageDetalhada.src = imagemClicada[0].src;
-			//console.log(b)
-
-			document.querySelector(".img-detalhada").appendChild(imageDetalhada);
-		btnFechar.onclick = () =>{
-			document.querySelector(".img-detalhada > img").remove();
-			document.querySelector(".tela-infs").classList.remove("sombra");
-			document.querySelector(".geral-produtos-inf").remove();
-		}
-		}
-
-	})
-
-}
